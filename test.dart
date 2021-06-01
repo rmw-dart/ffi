6baaa9c465c208fa3962c50d2649bb90797e283a
gcxfd <i@rmw.link>
2021-06-01 11:54:42 +0800

    🔶

--- test.dart
+++ test.dart
@@ -8,15 +8,18 @@ void main() {
   final ed25519 = Ed25519(seed);
   while (true) {
     final data = utf8.encoder.convert("$n");
+
     final sign = ed25519.sign(data);
-    print("$n ${sign.length} $sign");
+
     final h = hash(data);
     final hasher = Hasher();
     hasher.update(data);
     hasher.update(data);
     final h2 = hasher.end();
     if (n % 10000 == 0) {
-      print('$n ${base64.encode(h)} ${base64.encode(h2)}');
+      print('$n');
+      print('  ed25519 ${base64.encode(sign)}');
+      print('  ${base64.encode(h)} ${base64.encode(h2)}');
     }
     n++;
   }
